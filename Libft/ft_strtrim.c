52b1f7f32ec1b23ab6c281bf8d309680f9b0b0b7
Gaelle Ozon <gozon@made-f0Br4s12.clusters.42paris.fr>
2024-07-08 08:31:18 +0200

    Added libft

---
+++ Libft/ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 10:02:12 by gozon             #+#    #+#             */
+/*   Updated: 2024/05/22 10:21:02 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*trimmed;
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (start - end >= 0 && ft_strchr(set, s1[end]))
+		end --;
+	trimmed = ft_substr(s1, start, end - start + 1);
+	return (trimmed);
+}
