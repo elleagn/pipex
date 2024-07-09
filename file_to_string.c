1913d0267a976c77948229e5447cbffa2c9b284d
Gaelle Ozon <gozon@made-f0Br5s8.clusters.42paris.fr>
2024-07-09 09:08:43 +0200

    Finish f_to_str

--- file_to_string.c
+++ file_to_string.c
@@ -6,15 +6,15 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 13:36:57 by gozon             #+#    #+#             */
-/*   Updated: 2024/07/08 13:55:46 by gozon            ###   ########.fr       */
+/*   Updated: 2024/07/09 09:07:06 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-t_list	*ftolist(int fd)
+t_list	*f_to_list(int fd)
 {
-	char	*buffer;
+	t_list	*buffer;
 	t_list	*list;
 	int		readval;
 
@@ -22,17 +22,29 @@ t_list	*ftolist(int fd)
 	readval = -2;
 	while (readval)
 	{
-		buffer = malloc(10 * sizeof(char));
-		if (!buffer)
+		buffer = ft_lstnew(malloc(10 * sizeof(char)));
+		if (!buffer || !(buffer->content))
 		{
 			perror(NULL);
 			ft_lstclear(&list, free);
+			ft_lstclear(&buffer, free);
 			return (NULL);
 		}
+		readval = read(fd, buffer, 10);
+		if (readval == -1)
+			return (perror(NULL), ft_lstclear(&list, free),
+				ft_lstclear(&buffer, free), NULL);
+		ft_lstadd_back(&list, buffer);
 	}
+	return (list);
 }
 
-char	*ftostr(char *path)
+char	*list_to_str(t_list **list, int bufsize)
+{
+	char	*str;
+}
+
+char	*f_to_str(char *path)
 {
 	int		fd;
 	char	*str;
@@ -44,8 +56,8 @@ char	*ftostr(char *path)
 		ft_printf("%s : %s\n", path, strerror(errno));
 		return (NULL);
 	}
-	lst = ftolist(fd);
-	str = listjoin(lst);
+	lst = f_to_list(fd);
+	str = list_to_str(&lst);
 	ft_lstclear(&lst, free);
 	if (close(fd))
 	{
