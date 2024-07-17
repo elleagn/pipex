431fd73a0e32532881da94b6f658323df6e8ca49
Gaelle Ozon <gozon@made-f0Dr11s10.clusters.42paris.fr>
2024-07-17 18:27:46 +0200

    Semi-functionnal version of pipex

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 09:38:17 by gozon             #+#    #+#             */
-/*   Updated: 2024/07/15 10:38:18 by gozon            ###   ########.fr       */
+/*   Updated: 2024/07/17 14:17:41 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*get_path(char **envp)
+char	*get_path_var(char **envp)
 {
 	int		i;
 	char	*path;
@@ -33,21 +33,63 @@ char	*get_path(char **envp)
 	return (NULL);
 }
 
+char	**get_path_tab(char **envp)
+{
+	char	**path_tab;
+	char	*path;
+
+	path = get_path_var(envp);
+	if (!path)
+		return (NULL);
+	path_tab = ft_split(path, ':');
+	free(path);
+	if (!path_tab)
+		return (NULL);
+	return (path_tab);
+}
+
+// int	main(int ac, char **av, char **envp)
+// {
+// 	char	**path;
+
+// 	if (ac < 5)
+// 	{
+// 		ft_printf("argument list too short\n");
+// 		return (1);
+// 	}
+// 	path = get_path_tab(envp);
+// 	if (!path)
+// 		return (errno);
+// 	if (ft_strncmp("here_doc", av[1], 9))
+// 		pipex(ac - 3, av, path);
+// 	free_char_tab(path);
+// 	return (errno);
+// }
+
+// Test main
+
 int	main(int ac, char **av, char **envp)
 {
-	char	**path;
+	char		**path;
+	t_cl_args	*cl_args;
 
-	if (av < 5)
+	if (ac < 5)
 	{
 		ft_printf("argument list too short\n");
 		return (1);
 	}
-	path = get_path(envp);
+	path = get_path_tab(envp);
 	if (!path)
 		return (errno);
+	cl_args = malloc(sizeof(t_cl_args));
+	if (!cl_args)
+		return (free_char_tab(path), errno);
+	cl_args->cmd_nb = ac - 3;
+	cl_args->av = av;
+	cl_args->envp = envp;
 	if (ft_strncmp("here_doc", av[1], 9))
-		pipex(av, path, ac);
-	else
-		pipex_heredoc(av, path, ac);
+		pipex(cl_args, path);
+	free_char_tab(path);
+	free(cl_args);
 	return (errno);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 08:51:43 by gozon             #+#    #+#             */
-/*   Updated: 2024/07/16 10:32:29 by gozon            ###   ########.fr       */
+/*   Updated: 2024/07/17 14:09:22 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,21 @@
 # include <errno.h>
 # include <fcntl.h>
 # include <stdio.h>
+# include <sys/wait.h>
 # include "Libft/libft.h"
+
+typedef struct s_cl_args
+{
+	int		cmd_nb;
+	char	**av;
+	char	**envp;
+}				t_cl_args;
 
 int		**create_pipes(int size);
 char	**get_args(char *arg, char **path);
 void	close_pipes(int **pipes);
 void	free_int_tab(int **tab);
+void	free_char_tab(char **tab);
+int		pipex(t_cl_args *cl_args, char **path);
 
 #endif
